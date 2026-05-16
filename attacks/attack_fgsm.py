79b6d61f424baa4b3f07b0559314a31e6f6f6bd6
tusharvats2025 <tushar007vats@gmail.com>
2026-05-16 19:41:20 +0530

    feat: update FGSM attack signature with model and labels paramters.

--- attacks/attack_fgsm.py
+++ attacks/attack_fgsm.py
@@ -1,6 +1,6 @@
 import torch
 
-def fgsm_attack(image, epilon, data_grad):
+def fgsm_attack(model,image, labels, data_grad, epsilon, **kwargs):
     sign_data_grad = data_grad.sign()
-    perturbed_image = image + epilon * sign_data_grad
+    perturbed_image = image + epsilon * sign_data_grad
     return torch.clamp(perturbed_image, 0, 1)
--- attacks/init.py
+++ attacks/init.py
@@ -2,7 +2,7 @@ from attacks.attack_fgsm import fgsm_attack
 from attacks.attack_bim import bim_attack
 from attacks.attack_pgd import pgd_attack
 
-ATTACKS = {
+ATTACKS_REGISTORY = {
     "fgsm": fgsm_attack,
     "bim": bim_attack,
     "pgd": pgd_attack

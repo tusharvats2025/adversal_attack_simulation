620e05889c7c8c2233a4b7cbda11369fd567b545
tusharvats2025 <tushar007vats@gmail.com>
2026-05-16 22:01:13 +0530

    feat: add configurable image resizing support for MNIST loader.

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -1,13 +1,20 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
-def get_mnist(batch_size=64):
-    transform = transforms.ToTensor()
+def get_mnist(batch_size=64, image_size=28):
+    transform = transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.ToTensor(),
+    ])
 
-    train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
-    test_dataset = datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    train = datasets.MNIST(
+        "./data", train=True, download=True, transform=transform
+    )
+    test = datasets.MNIST(
+        "./data", train=False, download=True, transform=transform
+    )
 
-    train_loader= DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
-
-    return train_loader, test_loader
+    return(
+        DataLoader(train, batch_size=batch_size, shuffle=True),
+        DataLoader(test, batch_size=1, shuffle=False)
+    )

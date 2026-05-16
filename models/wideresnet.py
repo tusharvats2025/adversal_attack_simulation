eb7239aaf6fe3e900a4d403e0c1cb78eb4499571
tusharvats2025 <tushar007vats@gmail.com>
2026-05-17 00:43:46 +0530

    feat: standardize WideResNet constructor interface.

--- models/wideresnet.py
+++ models/wideresnet.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from torchvision.models import wide_resnet50_2
 
 class WideResNet(nn.Module):
-    def __init__(self, num_classes=200, in_channels=3):
+    def __init__(self, num_classes=200, in_channels=3, image_size=None, **kwargs):
         super().__init__()
         self.model = wide_resnet50_2(weights=None)
         self.model.conv1 = nn.Conv2d(

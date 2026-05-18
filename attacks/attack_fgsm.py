44691b1ca0f66c6f690e841c1dd58b66e131f191
tusharvats2025 <tushar007vats@gmail.com>
2026-05-18 20:58:08 +0530

    refractor(attacks): unify FGSM signature to match PGD/BIM - Removed data_grad paramter (now computes internally) - Added model parameter for consistency. - Signature now: (model, images, labels, epsilon, **kwargs)

--- attacks/attack_fgsm.py
+++ attacks/attack_fgsm.py
@@ -1,6 +1,29 @@
 import torch
 
-def fgsm_attack(model,image, labels, data_grad, epsilon, **kwargs):
+def fgsm_attack(model,images, labels, epsilon, **kwargs):
+    """
+    Fast Gradient Sign Method (FGSM) attack.
+
+    Args:
+        model    : Target model (unused in FGSM but kept for API consistency)
+        images   : Input images tensor
+        labels   : True labels
+        epsilon  : Perturbation magnitude.
+        **kwargs : Additional arguments (ignored).
+    Returns:
+       Adversarial images tensor
+    """
+
+    images.requires_grad_(True)
+
+    outputs = model(images)
+    loss = torch.nn.CrossEntropyLoss()(outputs, labels)
+
+    model.zero_grad()
+    loss.background()
+    
+    data_grad = images.grad.data
     sign_data_grad = data_grad.sign()
-    perturbed_image = image + epsilon * sign_data_grad
+    perturbed_image = images + epsilon * sign_data_grad
+    
     return torch.clamp(perturbed_image, 0, 1)

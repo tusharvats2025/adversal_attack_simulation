d9827d54e2e5f127c3d618ec062c58cfe528bed1
tusharvats2025 <tushar007vats@gmail.com>
2026-05-18 21:05:15 +0530

    fix(attacks): update ATTACKS_REGISTRY with unified signatures.

--- attacks/__init__.py
+++ attacks/__init__.py
@@ -1,9 +1,19 @@
 from attacks.attack_fgsm import fgsm_attack
 from attacks.attack_bim import bim_attack
 from attacks.attack_pgd import pgd_attack
+from attacks.autoattack_wrapper import run_autoattack
 
 ATTACKS_REGISTRY = {
     "fgsm": fgsm_attack,
     "bim": bim_attack,
-    "pgd": pgd_attack
+    "pgd": pgd_attack,
+    "autoattack": run_autoattack,
 }
+
+__all__ = [
+    "ATTACKS_REGISTRY",
+    "fgsm_attack",
+    "bim_attack", 
+    "pgd_attack",
+    "run_autoattack",
+]

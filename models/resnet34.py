cd5c3848d6f971bbb8a835b009f7eb987571256b
tusharvats2025 <tushar007vats@gmail.com>
2026-05-21 15:16:42 +0530

    fix(models): adjust ResNet34 conv1 kernel size for small images - Changed kernel_size form 7 to 3. - Removed intial maxpool for better spatial preservation.

--- models/resnet34.py
+++ models/resnet34.py
@@ -5,9 +5,11 @@ class ResNet34(nn.Module):
     def __init__(self, num_classes=200, in_channels=3, image_size=None, **kwargs):
         super().__init__()
         self.model = resnet34(weights=None)
+
         self.model.conv1 = nn.Conv2d(
-            in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False
+            in_channels, 64, kernel_size=3, stride=1, padding=1, bias=False
         )
+        self.model.maxpool = nn.Identity() # Remove intial maxpool
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):

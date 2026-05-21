93d2bb85d18762a73211720daf518f2dba78a1ae
tusharvats2025 <tushar007vats@gmail.com>
2026-05-21 15:13:39 +0530

    fix (models): adjust ResNet18 conv1 kernel size for small images. - Changed kernel_size from 7 to 3 for datasets under 64*64. - Keeps more spatial information for CIFAR-32 and MNIST-28.

--- models/resnet18.py
+++ models/resnet18.py
@@ -5,9 +5,12 @@ class ResNet18(nn.Module):
     def __init__(self, num_classes=10, in_channels=3, image_size=None, **kwargs):
         super().__init__()
         self.model = resnet18(weights=None)
+
+        # Adjust conv1 for small images (kernel 7 -> 3)
         self.model.conv1 = nn.Conv2d(
-            in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False
+            in_channels, 64, kernel_size=3, stride=1, padding=3, bias=False
         )
+        self.model.maxpool = nn.Identity() # Remove inital maxpool for small images.
         self.model.fc = nn.Linear(self.model.fc.in_features, num_classes)
 
     def forward(self, x):
